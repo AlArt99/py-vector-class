5e205ae885378f364b3031c9bc98c1c82e6f0d20
AlArt99 <AlArt1991@gmail.com>
2024-11-10 23:15:24 +0100

    created Vektor class, flake8 completed

--- app/main.py
+++ app/main.py
@@ -4,59 +4,48 @@ from math import sqrt, degrees, acos, cos, sin, radians
 
 class Vector:
 
-    def __init__(self, x: int | float, y: int | float) -> None:
-        self.x = round(float(x), 2)
-        self.y = round(float(y), 2)
+    def __init__(self, x_c: Union[int, float], y_c: Union[int, float]) -> None:
+        self.x = round(float(x_c), 2)
+        self.y = round(float(y_c), 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
-        )
+        return Vector(x_c=self.x + other.x, y_c=self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
-        )
+        return Vector(x_c=self.x - other.x, y_c=self.y - other.y)
 
     def __mul__(self, other: Union["Vector", int, float]) -> (
             Union)["Vector", int, float]:
-
         if isinstance(other, (int, float)):
-            return Vector(
-                x=self.x * other,
-                y=self.y * other
-            )
+            return Vector(x_c=self.x * other, y_c=self.y * other)
         elif isinstance(other, Vector):
             return (self.x * other.x) + (self.y * other.y)
         else:
             raise TypeError("Operand must be a Vector instance or a number.")
 
-    start_point = tuple()
-    end_point = tuple()
-
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple[float, float], end_point: tuple[float, float]) -> "Vector":
+    def create_vector_by_two_points(
+        cls, start_point: tuple[float, float], end_point: tuple[float, float]
+    ) -> "Vector":
         if len(start_point) < 2 or len(end_point) < 2:
-            raise ValueError("Both start_point and end_point must have at least two elements.")
+            raise ValueError(
+                "Both elements must have at least two elements."
+            )
+        x_cord = end_point[0] - start_point[0]
+        y_cord = end_point[1] - start_point[1]
+        return cls(x_cord, y_cord)
 
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-
-        return cls(x,y)
-
-    def get_length(self) -> int | float:
+    def get_length(self) -> Union[int, float]:
         return sqrt((self.x ** 2) + (self.y ** 2))
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         return Vector(
-            x=round(self.x / length, 2),
-            y=round(self.y / length, 2)
+            x_c=round(self.x / length, 2),
+            y_c=round(self.y / length, 2)
         )
 
-    def angle_between(self, other: "Vector") -> int :
+    def angle_between(self, other: "Vector") -> int:
         scalar_mult = (self.x * other.x) + (self.y * other.y)
         self_length = self.get_length()
         other_length = other.get_length()
@@ -71,9 +60,6 @@ class Vector:
 
     def rotate(self, degree: int) -> "Vector":
         rad_degree = radians(degree)
-        a = (self.x * cos(rad_degree)) - (self.y * sin(rad_degree))
-        b = (self.x * sin(rad_degree)) + (self.y * cos(rad_degree))
-        return Vector(
-            x=round(a, 2),
-            y=round(b, 2)
-        )
+        a_cord = (self.x * cos(rad_degree)) - (self.y * sin(rad_degree))
+        b_cord = (self.x * sin(rad_degree)) + (self.y * cos(rad_degree))
+        return Vector(x_c=round(a_cord, 2), y_c=round(b_cord, 2))
